be8396f10172db13553d500f4ce744bcfdb69d0f
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-05-28 23:16:01 +0530

    UPDATED: Arithmatic Operation UC 7

--- Operations.sh
+++ Operations.sh
@@ -1,13 +1,19 @@
 #! /bin/bash -x
 counter=0;
+counter2=0;
 
 declare -A Records;
+declare -a arrRecords;
 read -p "Enter First Number: " num1;
 read -p "Enter Second Number: " num2;
 read -p "Enter Third Number: " num3;
 comput=$((num1%num2));
 ans=$((comput+num3));
-Records[((counter++))]=$comput;
-Records[((counter++))]=$ans;
-echo "a%b+c="$ans;
-echo ${Records[@]};
+Records[((0))]=$comput;
+Records[((1))]=$ans;
+for((i=0;i<=${#Records[@]};i++))
+do
+	arrRecords[((counter++))]=$Records[i]
+done
+echo "Elements of Array: " ${arrRecords[@]};
+echo "Elements of Dictionary: " ${Records[@]};
