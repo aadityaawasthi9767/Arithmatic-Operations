a573e56cac0bacfe67ba40cc38101c4a5fd455d5
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-05-28 23:03:18 +0530

    UPDATED: Arithmatic Operations UC6

--- Operations.sh
+++ Operations.sh
@@ -1,7 +1,13 @@
 #! /bin/bash -x
+counter=0;
+
+declare -A Records;
 read -p "Enter First Number: " num1;
 read -p "Enter Second Number: " num2;
 read -p "Enter Third Number: " num3;
 comput=$((num1%num2));
 ans=$((comput+num3));
+Records[((counter++))]=$comput;
+Records[((counter++))]=$ans;
 echo "a%b+c="$ans;
+echo ${Records[@]};
