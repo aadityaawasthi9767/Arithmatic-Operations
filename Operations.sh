f2b9e8b29fa3be673412486ee3fc85d07f38e892
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-05-28 23:59:49 +0530

    UPDATED: Arithmatic Operations UC8

--- Operations.sh
+++ Operations.sh
@@ -3,17 +3,34 @@ counter=0;
 counter2=0;
 
 declare -A Records;
-declare -a arrRecords;
+
 read -p "Enter First Number: " num1;
 read -p "Enter Second Number: " num2;
 read -p "Enter Third Number: " num3;
-comput=$((num1%num2));
-ans=$((comput+num3));
-Records[((0))]=$comput;
-Records[((1))]=$ans;
-for((i=0;i<=${#Records[@]};i++))
+
+sum=$((num1+num2+num3));
+Records[0]=$sum;
+
+sub=$((num1-num2-num3));
+Records[((1))]=$sub;
+
+mult=$((num1*num2*num3));
+Records[((2))]=$mult;
+
+div=$((num1/num2/num3));
+Records[((3))]=$div;
+
+
+for(( i=0 ; i<=2 ; i++ ))
 do
-	arrRecords[((counter++))]=$Records[i]
+	for(( j=i+1 ; j<=((3)) ; j++ ))
+	do
+		if [ ${Records[ ((j)) ]} -le ${Records[ ((i)) ]} ]
+		then
+			temp=$Records[i];
+			Records[i]=$Records[j];
+			Records[j]=$temp;
+		fi
+	done
 done
-echo "Elements of Array: " ${arrRecords[@]};
-echo "Elements of Dictionary: " ${Records[@]};
+echo "Elements of Array: " ${Records[@]};
