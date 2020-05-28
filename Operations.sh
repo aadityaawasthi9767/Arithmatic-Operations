faa272977747d03bda8df0bba633a6fd3e9425ea
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-05-28 19:32:06 +0530

    UPDATED: Arithmatic Operation UC4

--- Operations.sh
+++ Operations.sh
@@ -2,4 +2,4 @@
 read -p "Enter First Number: " num1;
 read -p "Enter Second Number: " num2;
 read -p "Enter Third Number: " num3;
-echo "a*b+c="$((num1*num2+num3));
+echo "c+a/b="$((num3+num1/num2));
